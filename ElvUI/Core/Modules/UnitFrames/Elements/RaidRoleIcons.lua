ae8344e128d4fab6c307819dfd1e51cd2b041baf
coolmodi <7333074+coolmodi@users.noreply.github.com>
2021-12-05 16:31:18 +0100

    Enable Main Tank and Assist indicators for TBC

--- ElvUI/Core/Modules/UnitFrames/Elements/RaidRoleIcons.lua
+++ ElvUI/Core/Modules/UnitFrames/Elements/RaidRoleIcons.lua
@@ -9,6 +9,7 @@ function UF:Construct_RaidRoleFrames(frame)
 	frame.LeaderIndicator = anchor:CreateTexture(nil, 'OVERLAY')
 	frame.AssistantIndicator = anchor:CreateTexture(nil, 'OVERLAY')
 	frame.MasterLooterIndicator = anchor:CreateTexture(nil, 'OVERLAY')
+    frame.RaidRoleIndicator = anchor:CreateTexture(nil, 'OVERLAY')
 
 	anchor:Size(24, 12)
 	anchor:SetFrameLevel(frame.RaisedElementParent.RaidRoleLevel)
@@ -16,10 +17,12 @@ function UF:Construct_RaidRoleFrames(frame)
 	frame.LeaderIndicator:Size(12)
 	frame.AssistantIndicator:Size(12)
 	frame.MasterLooterIndicator:Size(12)
+    frame.RaidRoleIndicator:Size(12)
 
 	frame.LeaderIndicator.PostUpdate = UF.RaidRoleUpdate
 	frame.AssistantIndicator.PostUpdate = UF.RaidRoleUpdate
 	frame.MasterLooterIndicator.PostUpdate = UF.RaidRoleUpdate
+    frame.RaidRoleIndicator.PostUpdate = UF.RaidRoleUpdate
 
 	return anchor
 end
@@ -33,6 +36,7 @@ function UF:Configure_RaidRoleIcons(frame)
 			frame:EnableElement('LeaderIndicator')
 			frame:EnableElement('AssistantIndicator')
 			frame:EnableElement('MasterLooterIndicator')
+            frame:EnableElement('RaidRoleIndicator')
 		end
 
 		raidRoleFrameAnchor:ClearAllPoints()
@@ -42,6 +46,7 @@ function UF:Configure_RaidRoleIcons(frame)
 		frame:DisableElement('LeaderIndicator')
 		frame:DisableElement('AssistantIndicator')
 		frame:DisableElement('MasterLooterIndicator')
+        frame:DisableElement('RaidRoleIndicator')
 	end
 end
 
@@ -51,16 +56,19 @@ function UF:RaidRoleUpdate()
 	local leader = frame.LeaderIndicator
 	local assistant = frame.AssistantIndicator
 	local masterlooter = frame.MasterLooterIndicator
+    local raidRole = frame.RaidRoleIndicator
 
-	if not leader or not assistant or not masterlooter then return end
+	if not leader or not assistant or not masterlooter or not raidRole then return end
 
 	local db = frame.db
 	local isLeader = leader:IsShown()
 	local isAssist = assistant:IsShown()
+    local isRole = raidRole:IsShown()
 
 	leader:ClearAllPoints()
 	assistant:ClearAllPoints()
 	masterlooter:ClearAllPoints()
+    raidRole:ClearAllPoints()
 
 	if db and db.raidRoleIcons then
 		local pos, x, y = db.raidRoleIcons.position, db.raidRoleIcons.xOffset, db.raidRoleIcons.yOffset
@@ -69,14 +77,18 @@ function UF:RaidRoleUpdate()
 		local pos1 = right and 'RIGHT' or 'LEFT'
 		local pos2 = right and 'LEFT' or 'RIGHT'
 
-		if isLeader then
-			leader:Point(pos, anchor, x, y)
-			masterlooter:Point(pos1, leader, pos2)
-		elseif isAssist then
-			assistant:Point(pos, anchor, x, y)
-			masterlooter:Point(pos1, assistant, pos2)
-		else
-			masterlooter:Point(pos, anchor, x, y)
-		end
+        local elems = {}
+        if isLeader then table.insert(elems, leader) end
+        if isAssist then table.insert(elems, assistant) end
+        if isRole then table.insert(elems, raidRole) end
+        table.insert(elems, masterlooter)
+
+        for i = 1, #elems do
+            if i == 1 then
+                elems[i]:Point(pos, anchor, x, y)
+            else
+                elems[i]:Point(pos1, elems[i-1], pos2)
+            end
+        end
 	end
 end
